e84fd8e9e3200256a643dd6353423665154ed01b
SchneiderSeba <seba_19_sc@hotmail.com>
2024-08-18 16:06:11 +0200

    Controllers

---
+++ Controllers/movies.js
@@ -0,0 +1,56 @@
+import { MovieModel } from '../Models/movie.js'
+import { validateMovie, validatePartialMovie } from '../validateMovies.js'
+
+export class MoviesController {
+  static async getAll (req, res) {
+    const { genre } = req.query
+    const movies = await MovieModel.getAll({ genre })
+    res.json(movies)
+  }
+
+  static async getById (req, res) {
+    const { id } = req.params
+    const movie = await MovieModel.getById({ id })
+
+    if (movie) {
+      res.send(movie)
+    } else {
+      res.status(404).send({ error: 'Movie not found' })
+    }
+  }
+
+  static async create (req, res) {
+    const result = validateMovie(req.body)
+
+    if (result.error) {
+      return res.status(422).json({ error: JSON.parse(result.error.message) })
+    }
+    const newMovie = await MovieModel.create({ input: result.data })
+    res.status(201).json(newMovie)
+  }
+
+  static async delete (req, res) {
+    const { id } = req.params
+
+    const result = await MovieModel.delete({ id })
+
+    if (result === false) {
+      return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    return res.json({ message: 'Movie deleted' })
+  }
+
+  static async update (req, res) {
+    const { id } = req.params
+
+    const result = validatePartialMovie(req.body)
+    if (result.error) {
+      return res.status(422).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const updatedMovie = await MovieModel.update({ id, input: result.data })
+
+    return res.json(updatedMovie)
+  }
+}
--- Routes/movies.js
+++ Routes/movies.js
@@ -1,64 +1,20 @@
 import { Router } from 'express'
-import { validateMovie, validatePartialMovie } from '../validateMovies.js'
-import ACCEPTED_ORIGIN from '../originAccepted.js'
-import { MovieModel } from '../Models/movie.js'
+import { MoviesController } from '../Controllers/movies.js'
 
 export const movieRouter = Router()
 
-movieRouter.get('/:id', async (req, res) => {
-  const { id } = req.params
-  const movie = await MovieModel.getById({ id })
+movieRouter.get('/:id', MoviesController.getById)
 
-  if (movie) {
-    res.send(movie)
-  } else {
-    res.status(404).send({ error: 'Movie not found' })
-  }
-})
+movieRouter.get('/', MoviesController.getAll)
 
-movieRouter.get('/', async (req, res) => {
-  const origin = req.header('origin')
+// const origin = req.header('origin')
 
-  if (ACCEPTED_ORIGIN.includes(origin) || !origin) {
-    res.header('Access-Control-Allow-Origin', origin)
-  }
+// if (ACCEPTED_ORIGIN.includes(origin) || !origin) {
+//   res.header('Access-Control-Allow-Origin', origin)
+// }
 
-  const { genre } = req.query
-  const movies = await MovieModel.getAll({ genre })
-  res.json(movies)
-})
+movieRouter.post('/', MoviesController.create)
 
-movieRouter.post('/', async (req, res) => {
-  const result = validateMovie(req.body)
+movieRouter.patch('/:id', MoviesController.update)
 
-  if (result.error) {
-    return res.status(422).json({ error: JSON.parse(result.error.message) })
-  }
-  const newMovie = await MovieModel.create({ input: result.data })
-  res.status(201).json(newMovie)
-})
-
-movieRouter.patch('/:id', async (req, res) => {
-  const { id } = req.params
-
-  const result = validatePartialMovie(req.body)
-  if (result.error) {
-    return res.status(422).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const updatedMovie = await MovieModel.update({ id, input: result.data })
-
-  return res.json(updatedMovie)
-})
-
-movieRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-
-  const result = await MovieModel.delete({ id })
-
-  if (result === false) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  return res.json({ message: 'Movie deleted' })
-})
+movieRouter.delete('/:id', MoviesController.delete)
