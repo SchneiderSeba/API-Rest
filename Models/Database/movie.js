c4d7a4665285e6b3002736cc790f5176d6c10b5d
SchneiderSeba <seba_19_sc@hotmail.com>
2024-08-18 18:04:05 +0200

    omitir TLS y SSL

--- Models/Database/movie.js
+++ Models/Database/movie.js
@@ -1,6 +1,6 @@
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb'
 // const uri = 'mongodb+srv://seba19sc:OomQifbleNvAduCo@moviesapirest.xdwlu.mongodb.net/?retryWrites=true&w=majority&appName=MoviesAPIRest'
-const uri = 'mongodb+srv://seba19sc:FkCeXMdKp0QCmZDM@moviesapirest.xdwlu.mongodb.net/?retryWrites=true&w=majority&tls=true&ssl=true'
+const uri = 'mongodb+srv://seba19sc:FkCeXMdKp0QCmZDM@moviesapirest.xdwlu.mongodb.net/?retryWrites=true&w=majority&tlsInsecure=true'
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
