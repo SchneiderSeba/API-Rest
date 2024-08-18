c0b071ac8ab066d5a45050c8810c85cddd665694
SchneiderSeba <seba_19_sc@hotmail.com>
2024-08-18 17:41:44 +0200

    TLS permissessions.

--- Models/Database/movie.js
+++ Models/Database/movie.js
@@ -1,5 +1,6 @@
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb'
-const uri = 'mongodb+srv://seba19sc:OomQifbleNvAduCo@moviesapirest.xdwlu.mongodb.net/?retryWrites=true&w=majority&appName=MoviesAPIRest'
+// const uri = 'mongodb+srv://seba19sc:OomQifbleNvAduCo@moviesapirest.xdwlu.mongodb.net/?retryWrites=true&w=majority&appName=MoviesAPIRest'
+const uri = 'mongodb+srv://seba19sc:OomQifbleNvAduCo@moviesapirest.xdwlu.mongodb.net/?retryWrites=true&w=majority&appName=MoviesAPIRest&tls=true&tlsAllowInvalidCertificates=false'
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
