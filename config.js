ca62000ff52bb12a5d93a2ed005744a127d4e2ca
SchneiderSeba <seba_19_sc@hotmail.com>
2024-08-24 00:51:53 +0200

    .

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_HOST = process.env.DB_HOST || '127.0.0.1'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || ''
 export const DB_NAME = process.env.DB_NAME || 'railway'
