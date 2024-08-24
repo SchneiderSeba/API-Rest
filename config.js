1d30b8c6843127d88bb20708dfc36fceb3fa5d0b
SchneiderSeba <seba_19_sc@hotmail.com>
2024-08-24 14:49:05 +0200

    /

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const DB_HOST = process.env.DB_HOST || '127.0.0.1' || 'localhost' || 'moviesdb.railway.internal'
+export const DB_HOST = process.env.DB_HOST || 'junction.proxy.rlwy.net' || 'moviesdb.railway.internal' || '127.0.0.1' || 'localhost'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || ''
-export const DB_NAME = process.env.DB_NAME || 'railway'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'LqeWifYZVtpfKIJPktIdDpfLPgZGLFdq' || ''
+export const DB_NAME = process.env.DB_NAME || 'railway' || 'MoviesDB'
 export const DB_PORT = process.env.DB_PORT || 3306
