c288845bf754b99cd38eea88010308701c7837e7
SchneiderSeba <seba_19_sc@hotmail.com>
2024-08-27 22:19:54 +0200

    ./

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_HOST = process.env.DB_HOST || 'autorack.proxy.rlwy.net'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || ''
-export const DB_NAME = process.env.DB_NAME || 'moviesdb'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'jHhtfLfuwolufnZBlRcCKPhBjQtqcPRE'
+export const DB_NAME = process.env.DB_NAME || 'railway'
+export const DB_PORT = process.env.DB_PORT || 46132
